b0ec1ece2ce3f8168de52a9a9844463f8a82cba6
William <73823863+WilliamRdb@users.noreply.github.com>
2021-04-27 09:46:04 +0200

    NEW USER - Admin

--- init_data.sql
+++ init_data.sql
@@ -2,6 +2,7 @@ BEGIN;
 
 INSERT INTO "user" ("email","password","last_name","first_name","delivery_address","invoice_address","delivery_postcode","invoice_postcode","delivery_city","invoice_city","status","points","role","vegan") VALUES
 ('pipo@test.fr','1234','PIPO','PIPOPRENOM','5 rue Republique','6 rue République',11100,74000,'NARBONNE','VERSAILLES',true,1,'user',true),
+('admin@admin.fr','admin','admin','ADMIN','6 rue Republique','6 rue République',78370,78370,'PLAISIR','PLAISIR',false,0,'admin',false),
 ('autreadresse@test.fr','motdepasse','PIPO','JeanJacques','5 rue Republique','6 rue République',11100,74000,'NARBONNE','VERSAILLES',true,1,'user',true);
 
 INSERT INTO "promo"("code","start_date","end_date","pourcent")VALUES
